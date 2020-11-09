886fa5b971228e74cab0c0d9f9bfbb01c5aab662
KostraOne <guillaume.amangoua@gmail.com>
2020-11-09 15:03:09 +0100

    initial commit : welcome squad

---
+++ 0-day-zero/node-server.js
@@ -0,0 +1,8 @@
+console.log("hello world :) ");
+console.log("nice to meet you !!! ");
+console.log("nodemon rocks !!!");
+const o = {};
+
+o.name = "gui";
+console.log(o);
+console.log("loop ends");
