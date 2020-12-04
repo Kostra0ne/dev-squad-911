6b0566b8c329bb601bd328d3bf9bf08e0f9d7591
KostraOne <guillaume.amangoua@gmail.com>
2020-12-04 14:47:59 +0100

    added codealong todo list crud easy

--- module-2/week-1/day-5/2-todo-list-app/app.js
+++ module-2/week-1/day-5/2-todo-list-app/app.js
@@ -4,6 +4,7 @@ require("./config/database");
 const express = require("express");
 const hbs = require("hbs");
 const app = express();
+const TodoModel = require("./models/Todo");
 
 // setup
 // file allowed for browser download
@@ -16,9 +17,49 @@ app.set("view engine", "hbs");
 hbs.registerPartials(__dirname + "/views/partials");
 
 // let's code a  C.R.U.D APPLICATION
+
+// create todo for the todo list
+// read all todos
+// delete one todo
+// (update one todo)
+
 // route for the homepage
-app.get("/", (req, res) => {
-  res.send("todo list app");
+app.get("/", async (req, res) => {
+  const todos = await TodoModel.find();
+  res.render("todo", { todos });
+});
+
+app.post("/todo", async (req, res) => {
+  try {
+    await TodoModel.create(req.body);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.get("/todo/edit/:id", async (req, res) => {
+  const todoToEdit = await TodoModel.findOne({_id: req.params.id})
+  res.render("formEditTodo", todoToEdit);
+});
+
+app.post("/todo/edit/:id", async (req, res) => {
+  try {
+    await TodoModel.findByIdAndUpdate(req.params.id, req.body);
+    res.redirect("/");
+  } catch(err) {
+    console.log(err);
+  }
+});
+
+app.post("/todo/delete/:id", async (req, res) => {
+  try {
+    await TodoModel.findByIdAndRemove(req.params.id);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+  res.send("work-in-progress");
 });
 
 app.listen(process.env.PORT, () => {
