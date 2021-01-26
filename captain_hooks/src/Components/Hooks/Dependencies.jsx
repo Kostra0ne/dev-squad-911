99d92283a742c4faee8670d921b7c037ec3e0b5c
1testsample <frankomarmier@gmail.com>
2021-01-26 09:37:07 +0100

    Add captain hooks

---
+++ captain_hooks/src/Components/Hooks/Dependencies.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Container from "Components/Container";
+// import Button from "Components/Button";
+
+const Dependencies = (props) => {
+  return <Container></Container>;
+};
+
+export default Dependencies;
